b9dec8ba4bba9cfcbb37b9a27e260f1d6fd36ef4
elon21 <elongated21@gmail.com>
2021-05-08 22:27:57 +0300

    useState

--- src/ClassComponent.js
+++ src/ClassComponent.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
 class ClassComponent extends React.Component{
+    // constructor(){
+    //     super();
+    //     this.state = {
+    //         counter: 0
+    //     }
+    //     this.increase = this.increase.bind(this)
+    // }
+
+
+    state = {
+        counter: 0
+    }
+
+    increase = () => {
+        this.setState({counter: this.state.counter + 1})
+    }
+
+
     render() {
+        console.log("Class Counter: ", this.state.counter)
         return (
             <div className="class">
                 <h2>Class Component</h2>
+                <p>Counter: {this.state.counter}</p>
+                <button onClick={() => this.increase()}>Increase</button>
             </div>
         )
     }
--- src/FunctionalComponent.js
+++ src/FunctionalComponent.js
@@ -1,10 +1,21 @@
+import { useState } from "react";
 
 const FunctionalComponent = () => {
-    return (
-        <div className="function">
-            <h2>Functional Component</h2>
-        </div>
-    )
-}
+  const [counter, setCounter] = useState(0);
+
+  console.log("Func Counter: ", counter);
+
+  const increase = () => {
+      setCounter(counter+1)
+  }
+
+  return (
+    <div className="function">
+      <h2>Functional Component</h2>
+      <p>Counter: {counter}</p>
+      <button onClick={() => increase()}>Increase</button>
+    </div>
+  );
+};
 
 export default FunctionalComponent;
