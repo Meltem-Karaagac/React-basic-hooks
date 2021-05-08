66cd2645228ac38fa522ed22d073723ae0de925e
elon21 <elongated21@gmail.com>
2021-05-08 17:42:28 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ClassComponent from './ClassComponent';
+import FunctionalComponent from './FunctionalComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <ClassComponent />
+      <FunctionalComponent />
+    </div>
+  );
+}
+
+export default App;
